0e4510d140fd7e8fd11aa3824efae12e1aba570c
AJ <aqibjaveed.mad@gmail.com>
2026-04-18 18:58:54 +0500

    some updates

--- modules/custom/store_frontend/src/Controller/CartAccessController.php
+++ modules/custom/store_frontend/src/Controller/CartAccessController.php
@@ -6,16 +6,11 @@ use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Controller\ControllerBase;
 
-/**
- * Provides access checks for cart page.
- */
 class CartAccessController extends ControllerBase {
 
-  /**
-   * Checks access for cart page.
-   */
   public function access(AccountInterface $account) {
-    $allowed_roles = ['administrator', 'client'];
+    // Allowed roles: admin, client, and all member levels
+    $allowed_roles = ['administrator', 'client', 'member_level_1', 'member_level_2', 'member_level_3'];
     $user_roles = $account->getRoles();
     if (array_intersect($allowed_roles, $user_roles)) {
       return AccessResult::allowed();
